923c345248e6d045abb61ba48857c73a4e1b6c02
chrisrees222 <143870938+chrisrees222@users.noreply.github.com>
2023-09-10 19:49:49 +0100

    Update expressServer.js

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = new express();
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 let loginDetails = [];
 
@@ -20,7 +21,18 @@ app.get("/:name",(req,res)=>{
     res.send("Hello "+req.params.name)
 })
 
+app.get("/fetchMonth/:num",(req,res)=>{
+    let num = parseInt(req.params.num);
+    if(num <1 || num >12) {
+        res.send("Not a valid month number")
+    } else {
+        res.send(months[num-1])
+    }
+})
+
 app.listen(3333, () => {
     console.log(`Listening at http://localhost:3333`)
 })
+
+
 
